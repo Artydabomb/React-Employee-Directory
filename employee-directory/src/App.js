1c3ff73a4e4d4ef376d859d14c11f72c2a85a934
Arthur Leung <arthur.leung3@gmail.com>
2021-05-03 23:07:29 -0500

    header component built

---
+++ employee-directory/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import Title from './components/Title';
+import './components/Title.css';
+
+function App() {
+  return (
+    <div className="Title">
+      <Title />
+    </div>
+  );
+}
+
+export default App;
---
+++ employee-directory/src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Title() {
+    return (
+        <div>
+            <h1>Employee Directory</h1>
+            <div className="Title-Subtitle">Say hello to your work neighbor!</div>
+        </div>
+    )
+}
+
+export default Title
